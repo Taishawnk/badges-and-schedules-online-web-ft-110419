0ed3f0a8b962cb47401fb48f336db8566be02869
Taishawn King <taishawnking1@yahoo.com>
2019-11-08 01:37:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,23 +7,6 @@ def batch_badge_creator(attendees)
  return "Hello, my name is #{speaker}."}
   end
 
-def assign_room(speakers)
-  
-
-def printer(attendees)
-  batch_badge_creator(attendees).each {|message| puts message}
-  assign_rooms(attendees).each {|room| puts room}
-end
-
-
-def badge_maker(name)
-p "Hello, my name is #{name}."
-  end 
-
-def batch_badge_creator(attendees)
- attendees.collect {|name| p "Hello, my name is #{name}."}
-  end
-
 def assign_rooms(attendees)
   assigned = []
   counter = 1
